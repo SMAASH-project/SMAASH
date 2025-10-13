e4fd0576382f192907a06bfe607b95bf90769e45
BGERGO7 <colt-reopen-tree@duck.com>
2025-10-13 10:31:40 +0200

    #50 Visszaszámláló és Matchmaking változások
    Visszaszámláló hozzáadva, valamint a matchmakingnél nem lép be egyből a szobába, hanem megvárja a játékost, és csak ez után indítja el.
    
    Todo: Nagyon bugos a multplayer, össze-vissza ugrálnak a karakterek. Van amikor a samurai-t választom és amikor a szobába belép, akkor csak egy fekete képernyőt látok (valószínűleg nem adja hozzá a prefab-et a scene-hez).

--- Assets/Scripts/Multiplayer/CreateAndJoinRooms.cs
+++ Assets/Scripts/Multiplayer/CreateAndJoinRooms.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
+using Photon.Realtime;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-    public TMP_InputField createInput;
-    public TMP_InputField joinInput;
 
+    [SerializeField]
+    private int maxPlayers = 2;
+    private TMP_InputField createInput;
+    private TMP_InputField joinInput;
+
+    private bool isQuickMatch = false;
+
+    private void Start()
+    {
+        createInput = GameObject.Find("Create_input").GetComponent<TMP_InputField>();
+        joinInput = GameObject.Find("Join_input").GetComponent<TMP_InputField>();
+
+        // ensure the master client's LoadLevel call is propagated to all clients
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
     
     //Szoba letrehozasa
     public void CreateRoom()
     {
-        PhotonNetwork.CreateRoom(createInput.text);
+        if(isQuickMatch)
+        {
+            Debug.Log("Creating Quick Match Room");
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.MaxPlayers = (byte)maxPlayers;
+            PhotonNetwork.CreateRoom(null, roomOptions, null);
+        }
+        else
+        {
+            Debug.Log("Creating Room: " + createInput.text);
+            // ensure named rooms also have proper max players
+            RoomOptions ro = new RoomOptions { MaxPlayers = (byte)maxPlayers };
+            PhotonNetwork.CreateRoom(createInput.text, ro, null);
+        }
     }
 
     //Szobahoz csatlakozas
@@ -23,9 +50,40 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinRoom(joinInput.text);
     }
 
+    public void QuickMatch()
+    {
+        //SceneManager.LoadScene("sc_loading", LoadSceneMode.Single);
+        isQuickMatch = true; // set before join so CreateRoom knows
+        PhotonNetwork.JoinRandomRoom();
+        //SceneManager.LoadScene("sc_loading");
+
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        CreateRoom();
+    }
+
     //Miutan csatlakozott a szobahoz, belep a palyara
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("sc_main");
+        Debug.Log("Players in room: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        if(PhotonNetwork.CurrentRoom.PlayerCount == maxPlayers)
+        {
+            Debug.Log("Loading Level");
+            PhotonNetwork.LoadLevel("sc_main");
+            
+        }
+    }
+
+    // ensure the room creator (master client) loads the scene when the second player joins
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        Debug.LogFormat("Player entered: {0}. Now {1}/{2}", newPlayer.NickName ?? newPlayer.UserId, PhotonNetwork.CurrentRoom.PlayerCount, PhotonNetwork.CurrentRoom.MaxPlayers);
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == maxPlayers)
+        {
+            Debug.Log("Master detected required players -> loading scene for all clients");
+            PhotonNetwork.LoadLevel("sc_main");
+        }
     }
 }
